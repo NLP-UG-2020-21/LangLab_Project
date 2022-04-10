8909a354c43fc472b44e094741d3499b8f301439
Krystian Borkowski <krystianborkowski1998@gmail.com>
2022-04-10 15:14:09 +0200

    Updated contact.html, contact.js and nav.css (hover and messaging repaired)

--- js/contact.js
+++ js/contact.js
@@ -3,5 +3,5 @@ document.querySelector('.contact__form').addEventListener('submit', (ev) => {
     const name = document.querySelector('.contact__input--name').value;
     const email = document.querySelector('.contact__input--email').value;
     const msg = document.querySelector('.contact__input--message').value;
-    window.open(`mailto:test@test.test?subject=${name}&body=${msg}`, '_self');
+    window.open(`mailto:calmebot.langlab@gmail.com?subject=${name}&body=${msg}`, '_self');
 });
