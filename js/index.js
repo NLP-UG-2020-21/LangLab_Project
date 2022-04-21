b1343a18c1aa144c29d6af9d906608071a3d649d
Krystian Borkowski <krystianborkowski1998@gmail.com>
2022-04-21 20:17:36 +0200

    First implementation of the new chatbox, copy of previous index.js archived for future

--- js/index.js
+++ js/index.js
@@ -1,113 +1,94 @@
-let messages = [], //array that hold the record of each string in chat
-    lastUserMessage = '', //keeps track of the most recent input string from the user
-    botMessage = '', //var keeps track of what the chatbot is going to say
-    botName = 'Eva'; //name of the chatbot
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
 
-const sendButton = document.getElementById('send-button'); 
-const submitButton = document.getElementById('submit-button');
+const BOT_MSGS = [
+    "Hi, how are you?",
+    "Ohh... I can't understand what you trying to say. Sorry!",
+    "I like to play games... But I don't know how to play!",
+    "Sorry if my answers are not relevant. :))",
+    "I feel sleepy! :("
+];
 
-//edit this function to change what the chatbot says
-const chatbotResponse = () => {
-    let userName = document.getElementById('name-box').value;
-    let urlUserMessage = lastUserMessage.replace('/s/g', '%20');
-    for (let i = 1; i < 8; i++) {
-        if (messages[messages.length - i]) {
-            document.getElementById('chatlog' + i).innerHTML =
-                messages[messages.length - i];
-        }
-    }
-    Promise.all([
-        fetch(
-            'https://ai-chatbot.p.rapidapi.com/chat/free?message=' +
-                urlUserMessage +
-                '%3F&uid=' +
-                userName,
-            {
-                method: 'GET',
-                headers: {
-                    'x-rapidapi-host': 'ai-chatbot.p.rapidapi.com',
-                    'x-rapidapi-key':
-                        'c78749d9f1msh0f8f753e4481a33p1d4b75jsn9fb4ac975528',
-                },
-            }
-        ),
-        new Promise((resolve) =>
-            setTimeout(() => resolve(''), Math.random() * 1000 + 1000)
-        ),
-    ])
-        .then((response) => response[0].json())
-        .then((response) => {
-            //outputs the last few array elements of messages to html
+// Icons made by Freepik from www.flaticon.com
+const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
+const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
+const BOT_NAME = "BOT";
+const PERSON_NAME = "Sajad";
 
-            botMessage = response['chatbot']['response'];
-            //add the chatbot's name and message to the array messages
+msgerForm.addEventListener("submit", event => {
+    event.preventDefault();
 
-            messages.push('<b>' + botName + ':</b> ' + botMessage);
-            //outputs the last few array elements of messages to html
-            for (let i = 1; i < 8; i++) {
-                if (messages[messages.length - i]) {
-                    document.getElementById('chatlog' + i).innerHTML =
-                        messages[messages.length - i];
-                }
-            }
-        });
-};
-
-//this runs each time enter is pressed.
-//It controls the overall input and output
-const newEntry = () => {
+    const msgText = msgerInput.value;
+    if (!msgText) return;
     let userName = document.getElementById('name-box').value;
     if (userName === '') {
         alert('You need to input your name!');
     } else {
-        //if the message from the user isn't empty then run
-        if (document.getElementById('chatbox').value !== '') {
-            //pulls the value from the chatbox ands sets it to lastUserMessage
-            lastUserMessage = document.getElementById('chatbox').value;
-            //sets the chat box to be clear
-            document.getElementById('chatbox').value = '';
-            //adds the value of the chatbox to the array messages
-            messages.push(lastUserMessage);
-            //sets the variable botMessage in response to lastUserMessage
-            chatbotResponse();
-        }
-    }
-};
+        appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+        msgerInput.value = "";
 
-// if the key pressed is 'enter' runs the function newEntry()
-const keyPress = (event) => {
-    const x = event || window.event;
-    const key = x.keyCode || x.which;
-    if (key === 13 || key === 3) {
-        //runs this function when enter is pressed
-        newEntry();
+        botResponse();
     }
-    if (key === 38) {
-        document.getElementById('chatbox').value = lastUserMessage;
-    }
-};
+});
 
-sendButton.onclick = (event) => newEntry();
+function appendMessage(name, img, side, text) {
+    //   Simple solution for small apps
+    const msgHTML = `
+    <div class="msg ${side}-msg">
+        <div class="msg-img" style="background-image: url(${img})"></div>
 
-//clears the placeholder text ion the chatbox
-//this function is set to run when the users brings focus to the chatbox, by clicking on it
-function placeHolder() {
-    document.getElementById('chatbox').placeholder = '';
+        <div class="msg-bubble">
+        <div class="msg-info">
+            <div class="msg-info-name">${name}</div>
+            <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+        <div class="msg-text">${text}</div>
+        </div>
+    </div>
+    `;
+    msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+    msgerChat.scrollTop += 500;
 }
+
+function botResponse() {
+    const r = random(0, BOT_MSGS.length - 1);
+    const msgText = BOT_MSGS[r];
+    const delay = msgText.split(" ").length * 100;
+
+    setTimeout(() => {
+        appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
+    }, delay);
+}
+
+// Utils
+function get(selector, root = document) {
+    return root.querySelector(selector);
+}
+
+function formatDate(date) {
+    const h = "0" + date.getHours();
+    const m = "0" + date.getMinutes();
+
+    return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+// disables submit button on click
+submitButton.addEventListener('click', function(event) {
+    event.target.disabled = true;
+});
 
 // hides text input upon clicking on submit button
 function toggle() {
     let element = document.getElementById('name-box');
-  
-    if ( element.style.display!='none' ) {
+
+    if ( element.style.display!=='none' ) {
       element.style.display='none';
     } else {
       element.style.display='';
     }
-  }
-// disables submit button on click
-  submitButton.addEventListener('click', function(event) {
-      event.target.disabled = true;
-  });
-// runs the keypress() function when a key is pressed
-document.onkeypress = keyPress;
+}
